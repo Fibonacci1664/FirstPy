4d9c166e97fa3a74e4dd9ead8733aee64495e4f0
Fibonacci <dave.green1664@yahoo.co.uk>
2020-06-04 17:16:25 +0100

    Learned about argument, parameters and defaults.

--- source/Main.py
+++ source/Main.py
@@ -1,8 +1,26 @@
 
 
-def my_function():
-    print("This is my first py func!")
-    print("This is another print line.")
+# def my_function(str1, str2):
+#     print(str1)
+#     print(str2)
+#
+#
+# my_function("This is arg 1", "This is arg 2.")
+# my_function("This is another arg", "This is a further arg")
 
 
-my_function()
+def print_something(name = "Default", age = "Unknown"):
+    print("My name is ", name, " and my age is ", age)
+
+
+print_something("Dave", 39)
+
+print_something("Dave")
+
+print_something(None, 39)
+
+print_something(age = 39, name = "Dave")
+
+print_something(age = 39)
+
+
