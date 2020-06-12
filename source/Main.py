b899833896926bd2f54d117e7408f641e9c6982c
Fibonacci <dave.green1664@yahoo.co.uk>
2020-06-12 15:35:38 +0100

    Learned about infinite parameters and returning from a function.

--- source/Main.py
+++ source/Main.py
@@ -1,5 +1,3 @@
-
-
 # def my_function(str1, str2):
 #     print(str1)
 #     print(str2)
@@ -9,18 +7,32 @@
 # my_function("This is another arg", "This is a further arg")
 
 
-def print_something(name = "Default", age = "Unknown"):
+def print_something(name="Default", age="Unknown"):
     print("My name is ", name, " and my age is ", age)
 
 
-print_something("Dave", 39)
-
-print_something("Dave")
-
-print_something(None, 39)
-
-print_something(age = 39, name = "Dave")
-
-print_something(age = 39)
+def print_people(*people):  # The asterix says that this will be an array of
+    for person in people:  # all the arguments passed in to the function.
+        print("This person is: ", person)
 
 
+def do_math(num1, num2):
+    return num1 + num2
+
+
+math_1 = do_math(5, 7)
+math_2 = do_math(11, 34)
+
+print("First sum = ", math_1, " Second sum = ", math_2)
+
+print_people("Nick", "Dan", "Bob", "Smiley")
+
+# print_something("Dave", 39)
+#
+# print_something("Dave")
+#
+# print_something(None, 39)
+#
+# print_something(age=39, name="Dave")
+#
+# print_something(age=39)
